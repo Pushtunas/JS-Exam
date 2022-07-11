313d84b00eba6f02a458e9ffcefb41e6df42058b
Edvardas <edvardas.giedrys@codeacademy.lt>
2022-07-12 01:16:36 +0300

    baigtas 3 task'as

--- Task 3/script.js
+++ Task 3/script.js
@@ -13,17 +13,54 @@ turėti bent minimalų stilių ir būti responsive;
 
 const ENDPOINT = 'https://api.github.com/users';
 
-const rezultatai = document.querySelector('#output');
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', gettData);
+// const rezultatai = document.querySelector('#output');
+// const btn = document.querySelector('#btn');
+// btn.addEventListener('click', gettData);
 
-function gettData() {
+// function gettData() {
+//     fetch(ENDPOINT).then(function (res) {
+//         return res.json();
+//     }).then(function (data) {
+//         console.log(data);
+//         duomenys(data)
+//     }).catch(function (e) {
+//         console.log(e);
+//     })
+// }
+
+
+// function duomenys (arr) {
+//     arr.forEach(elementas =>{
+//         console.log(elementas.login);
+//         console.log(elementas.avatar_url);
+//         let elCard = document.createElement('div'); //sukurem tuscia elemnta
+//         elCard.innerHTML = `${elementas.login} ${elementas.avatar_url}`
+//         rezultatai.appendChild(elCard)
+//     });
+// }
+
+const hideP = document.querySelector('#message');
+const results = document.querySelector('#output');
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', getData);
+
+function getData() {
+
     fetch(ENDPOINT).then(function (res) {
         return res.json();
     }).then(function (data) {
-        console.log(data);
-    }).catch(function (e) {
-        console.log(e);
+        // console.log(data.results);
+        data.forEach(el => {
+            let userCard = document.createElement('div');
+            userCard.textContent = `${el.login}`;
+            let avatar = document.createElement('img');
+            avatar.setAttribute('src', el.avatar_url);
+            avatar.style.display = 'block';
+            userCard.appendChild(avatar);
+            results.appendChild(userCard);
+            userCard.classList.add('userCard');
+        });
+    }).then(function () {
+        hideP.classList.add('hide');
     })
 }
-
