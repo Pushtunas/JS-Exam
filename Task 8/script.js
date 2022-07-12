fb1c7afe2c9e4c4960cf113b134e49e029b61db1
Edvardas <edvardas.giedrys@codeacademy.lt>
2022-07-12 21:58:31 +0300

    pradetas bet blogai

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,20 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+// pasinaudoti  konstruktoriaus funkcija: function... 15 uzduotis
+
+
+function Calculator(a, b) {
+    this.a = Number(a);
+    this.b = Number(b);
+}
+
+// negerai cia viskas
+
+let sum = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+
+const numbers = new Calculator(5, 3);
+console.log(numbers.sum(), numbers.subtraction(), numbers.multiplication(), numbers.division(),);
